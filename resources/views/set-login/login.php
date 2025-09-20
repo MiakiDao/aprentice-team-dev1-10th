be65d6b6ce687e422dfb6b66aae9fd2079f8d94a
Megumi Kishibe <Kishibe0417@gmail.com>
2025-09-20 19:45:39 +0900

    login: 二重フォーム削除＆閉じタグ修正; router/スタイル微調整; add home.css

--- resources/views/set-login/login.php
+++ resources/views/set-login/login.php
@@ -6,7 +6,7 @@ session_start();
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>login画面UIのみ</title>
+  <title>login画面</title>
   <link rel="stylesheet" href="/css/styleset-home.css" />
   <link rel="stylesheet" href="/css/start.css" />
   <link rel="stylesheet" href="/css/login.css" />
@@ -44,56 +44,6 @@ session_start();
         <div class="container">© 2025 TeamProject1</div>
       </footer>
     </div>
-    <!-- banner ---------------------------------------------->
-    <div class="banner">
-      <h2>食事管理アプリ | ログイン</h2>
-    </div>
-
-    <!-- main ---------------------------------------------->
-    <div class="main wrapper">
-      <div class="content">
-
-        <!-- 入力されたデータが一致しない場合はSigninControllerから発行されたエラー文を表示 -->
-        <?php if (!empty($_SESSION['error'])): ?>
-        <p style="color:red;"><?php echo $_SESSION['error']; ?></p>
-        <?php unset($_SESSION['error']); ?>
-        <?php endif; ?>
-
-
-        <!-- バリデーション入れる -->
-        <form action="/index.php?page=signin_verify" method="POST">
-          <div class="form">
-            <label for="">メールアドレス</label>
-            <input type="email" name="email" id="email">
-          </div>
-          <div class="form">
-            <label for="">パスワード　　</label>
-            <input type="password" name="password" id="password">
-          </div>
-
-
-
-        <div class="register">
-          <nav class="main-nav">
-            <ul>
-              <li class="login">
-                <button style="all: unset;" type="submit">ログイン</button>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        </form>
-      </div>
-    </div>
-
-    <!-- footer ---------------------------------------------->
-
-    <footer id="footer">
-      <div class="footer wrapper">
-        <div class="footer-logo"><a href="/"><img src="/image/logo.png" alt="ロゴ"></a></div>
-        <p>Copyright © TeamDev1 食事記録アプリ</p>
-      </div>
-    </footer>
   </div>
     <script src="/js/login.js"></script>
 </body>
